5a759c67156f5399cc6bffa1f53b4248a41bd011
BonelessCode <stevenener@live.fr>
2024-06-23 21:31:23 +0200

    28/02/2020 - Refactored the code to improve readability and maintainability. Improved the efficiency of the Combinaison function using recursion. Added functions to calculate the distances between all points and create a square zero matrix. Made the code more generic by removing hardcoded values. Added comments to explain the code.

--- SCRIPT.py
+++ SCRIPT.py
@@ -1,90 +1,120 @@
 from math import *
 from random import randint
 from copy import deepcopy
-pt=((0,0),(1,1),(2,2),(3,3),(4,4),(5,5))
+pt=((0,0),(1,1),(2,2),(3,3),(4,4),(5,5),(6,6))
 n=len(pt)
 d_terme=n-2
 dt_range=n-1
 
 def Empty_List(n):
-    """créer une double liste (matrice carrée) nulle de taille n (entier naturel >=3)"""
+    """creer une double liste (matrice carree) nulle de taille n (entier naturel superieur ou egal a 3)"""
     inside=[[0 for j in range(n )] for i in range(n)]
     return inside
 
 
-def Dernier_Terme(chiffres_restant,dt_range):
-    """"""
+def Dernier_Terme(chiffres_restant):
+    """
+    chiffres_restant: Tableau a deux dimensions
+    Determine le dernier terme d'un tableau qui n'est pas une liste vide.
+    (A l'envers pour optimiser car la plupart du temps ce terme sera a la fin.)
+    """
     p_max_test=0
-    for compteur_test in range(0,dt_range):
-        # Cherche la dernière liste non vide
-        if chiffres_restant[compteur_test]!=[]:
-            p_max_test=compteur_test
+    len_range=len(chiffres_restant)
+    for compteur_test in range(0,len_range):
+        if chiffres_restant[len_range-1-compteur_test]!=[]:
+            p_max_test=len_range-1-compteur_test
+            return p_max_test
 
-    return p_max_test
+def Combinaison(choix_actuel,chiffres_restant,n):
+    """
+    n entier naturel (int) qui correspond au nombre de points
+    choix_actuel: Liste de taille n-1
+    chiffres_restant: Tableau a deux dimensions
 
-# Ensemble des cominaisons à renvoyer
+    Fonction principale, utilise la recursivite.
+    Permet de determiner toutes les combinaisons possibles de chemin afin de resoudre le probleme.
+    S'arrete lorsqu'on a atteint la derniere combinaison
+    """
+
+
+    global pos_max
+    """Cette variable est utilisee dans les differentes repetition de la fonction
+    et on souhaite pouvoir la recuperer et avoir la meme quelque soit l'instant de la fonction"""
+    dt_range=len(chiffres_restant)
+    # Si on a pas encore atteint le dernier terme
+    if chiffres_restant!=[[] for k in range(dt_range)]:
+        # Recupere la position de la derniere liste non vide
+        pos_max=Dernier_Terme(chiffres_restant)
+
+        # Rafraichissement des donnees
+        chiffres_restant[pos_max].remove(choix_actuel[pos_max])
+
+        # Recursion
+        if chiffres_restant[pos_max]==[]:
+            choix_actuel,chiffres_restant=Combinaison(choix_actuel,chiffres_restant,n)
+
+        # Si on en est pas au dernier terme
+        if choix_actuel!=-1:
+            # Reinitialisaition des valeurs possibles pour les chiffres suivants
+            for k in range(pos_max+1,dt_range):
+                chiffres_restant[k]=[o for o in range(2,n+1)]
+                for j in range(0,k):
+                    chiffres_restant[k].remove(choix_actuel[j])
+
+                choix_actuel[k]=chiffres_restant[k][0]
+
+
+            choix_actuel[pos_max]=chiffres_restant[pos_max][0]
+
+            return choix_actuel,chiffres_restant
+
+
+    """ If et pas else car on doit pouvoir sortir de la fonction
+    en renvoyant les valeurs adequate et donc cette condition doit etre testee apres etre sorti d'une possible plongee recursive"""
+    if chiffres_restant==[[] for k in range(dt_range)]:
+        # Valeur speciale de choix_actuel permettant de terminer le programme
+        return (-1,chiffres_restant)
+
+
+
+
+
+# Ensemble des cominaisons a renvoyer
 combi=[]
-backup=0
 pos_max=4
 
-# servira pour créer les combinaisons
+# servira pour creer les combinaisons
 chiffres_restant=[[j for j in range(2,n+1)] for x in range(dt_range)]
 choix_actuel=[0 for i in range(dt_range)]
 
-# Construction de la première Combinaison choisie
+# Construction de la premiere Combinaison choisie
 for j in range(dt_range):
-    #choix_actuel[j]=chiffres_restant[j][randint(0,len(chiffres_restant[j])-1)]
-
     choix_actuel[j]=chiffres_restant[j][0]
 
-    # Enlève les chiffres déjà utilisés pour les combinaisons suivantes et empêche de dépasser le dernier terme du range
+    # Enleve les chiffres deja utilises pour les combinaisons suivantes et empeche de depasser le dernier terme du range
     for m in range(min(j+1,dt_range),dt_range):
         chiffres_restant[m].remove(choix_actuel[j])
 
-
-while chiffres_restant[0]!=[]:
-    print(choix_actuel)
+# Valeur que prendra choix_actuel a la fin (vois fonction Combinaison)
+while choix_actuel!=-1:
+    # Ajout dans la liste des combinaison
+    combi.append([1]+choix_actuel+[1])
     print(chiffres_restant)
+    choix_actuel=Combinaison(choix_actuel,chiffres_restant,n)[0]
 
-    if backup != choix_actuel:
-        combi.append(choix_actuel)
-        backup=deepcopy(choix_actuel)
-
-
-
-    # S'assurer que l'ensemble est toujours non vide
-    pos_max=Dernier_Terme(chiffres_restant,dt_range)
-
-    # Rafraichissement des données
-    choix_actuel[pos_max]=chiffres_restant[pos_max][0]
-    chiffres_restant[pos_max].remove(choix_actuel[pos_max])
-
-
-    # Reinitialisaition des valeurs possibles pour les chiffres suivants
-    # Permet de compenser les vides
-    for k in range(pos_max+1,dt_range):
-        chiffres_restant[k]=[o for o in range(2,n+1)]
-        print(k,"numero")
-        for j in range(0,k):
-            chiffres_restant[k].remove(choix_actuel[j])
-
-        choix_actuel[k]=chiffres_restant[k][0]
-        print(choix_actuel[k],"valeur")
-
-
-
+print("Voici la liste des combinaisons: \n",combi)
 nb_resultat=len(combi)
-print(nb_resultat)
-print(combi)
+print("Nombre de resultats: ",nb_resultat)
 distance=Empty_List(n)
 
-# Création de la matrice comportant les distances
+# Creation de la matrice comportant les distances
 for i in range(n):
     for j in range(n):
          #if i !=j:
         distance[i][j]=int(sqrt(((pt[i][0]-pt[j][0])**2)+(pt[i][1]-pt[j][1])**2))
 
 
-# Affiche la matrice
-#for i in range(n):
-#    print([distance[i][j] for j in range(n)])
+#Affiche la matrice
+print("Tableau des distances entre chaques points:")
+for i in range(n):
+    print([distance[i][j] for j in range(n)])
